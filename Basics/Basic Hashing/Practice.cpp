8bd70d07706e094d89b63afa89f5a9f255361c6c
Aadish-jain <aadishjain0980@gmail.com>
2023-02-16 19:18:26 +0530

    Basic Hashing (Part 2)

---
+++ Basics/Basic Hashing/Practice.cpp
@@ -0,0 +1,38 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//  int n;
+//  cin >> n;
+//  map <int, int, greater<int>> mpp;
+//  multimap <int, int, greater<int>> mpp1;
+//  int arr[n];
+//  for(int i = 0; i < n; i++){
+//   cin >> arr[i];
+//   mpp[arr[i]]++;
+//  }
+
+//  for(int i = 0; i < n; i++){
+//   cout << arr[i] << " ";
+//  }
+//  cout << endl;
+//  for (auto it : mpp){
+//   cout << it.first << "->" << it.second << endl;
+//   mpp1.insert({it.second, it.first});
+//  }
+//  cout << endl;
+//  for (auto it1 : mpp1){
+//   cout << it1.first << "->" << it1.second << endl;
+  
+//  }
+//  int k;
+//  cin >> k;
+
+//  for(int i = 0; i < k; i++){
+//   cout << mpp1[i] << " ";
+//  }
+
+ 
+
+//  return 0;
+// }
